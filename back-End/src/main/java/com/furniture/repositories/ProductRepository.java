4accd5a90531e63335d8cd1328c2582f1fedbb78
Sevi68 <sevindupunsara313@gmail.com>
2024-03-03 00:48:52 +0530

    Changee Product files

--- back-End/src/main/java/com/furniture/repositories/ProductRepository.java
+++ back-End/src/main/java/com/furniture/repositories/ProductRepository.java
@@ -3,7 +3,7 @@ package com.furniture.repositories;
 import com.furniture.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
     // You can define additional query methods here if needed
 }
 
