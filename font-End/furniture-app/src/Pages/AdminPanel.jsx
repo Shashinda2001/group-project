02164ff46c3f7b5dec260637a8a3bf419ae3aa82
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-03-03 01:15:39 +0530

    admin panal update

--- font-End/furniture-app/src/Pages/AdminPanel.jsx
+++ font-End/furniture-app/src/Pages/AdminPanel.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-<<<<<<< Updated upstream
-=======
+ 
 import ProductAdd from '../Component/ProductAdd';
 // import "../css/admin.css"
 import ProductList from "../Component/ProductList";
->>>>>>> Stashed changes
+ 
 
 function AdminPanel() {
     return ( 
         <div>
-<h1>hello pakooo</h1>
+ <ProductAdd/>
+ <ProductList/>
         </div>
         
      );
