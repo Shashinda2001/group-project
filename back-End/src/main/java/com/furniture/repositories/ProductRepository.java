af1eda722c684d300ea525fc99692772e0925e8e
Sevi68 <sevindupunsara313@gmail.com>
2024-03-02 23:53:44 +0530

    Deleted product files

--- back-End/src/main/java/com/furniture/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.furniture.repositories;
-
-import com.furniture.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-}
